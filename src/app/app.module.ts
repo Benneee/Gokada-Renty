3ed1eea0804becdea660c758086164d8d0051699
Benneee <benedictiknkeonye@gmail.com>
2019-06-29 14:50:38 +0100

    completed sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-
-
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { SearchComponent } from './search/search.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { SearchComponent } from "./search/search.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    SearchComponent
-  ],
-  imports: [
-    BrowserModule,
-    AngularFontAwesomeModule
-  ],
+  declarations: [AppComponent, NavComponent, SearchComponent],
+  imports: [BrowserModule, AngularFontAwesomeModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
